1d820cbac18ecad46c1489ebbd251ae387c0af06
joxy-php <138411168+joxy-php@users.noreply.github.com>
2024-10-31 15:05:14 +0100

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,3 @@
+@echo off
+mshta "javascript:var sh=new ActiveXObject('WScript.Shell'); sh.Popup('Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards, Albanian virus', 0, 'Virus Alert!', 64);close();"
+exit
